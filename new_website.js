3f0fe79936d865769d8dbe73d8321b4ca1292ce2
Abhinav <abhi.abhinav444@gmail.com>
2020-07-15 19:33:53 +0530

    commit

--- new_website.js
+++ new_website.js
@@ -1,31 +1,28 @@
-let date=document.getElementById('date');
-function updateclock()
-{
-    let time=new Date();
-    let currhr=time.getHours();
-    if(currhr<10)
-    currhr="0"+currhr;
-    let currmin=time.getMinutes();
-    if(currmin<10)
-    currmin="0"+currmin;
-    let currsec=time.getSeconds();
-    if(currsec<10)
-    currsec="0"+currsec;
-    date.innerHTML=currhr+":"+currmin+":"+currsec;
+let date = document.getElementById('date');
+function updateclock() {
+  let time = new Date();
+  let currhr = time.getHours();
+  if (currhr < 10)
+    currhr = "0" + currhr;
+  let currmin = time.getMinutes();
+  if (currmin < 10)
+    currmin = "0" + currmin;
+  let currsec = time.getSeconds();
+  if (currsec < 10)
+    currsec = "0" + currsec;
+  date.innerHTML = currhr + ":" + currmin + ":" + currsec;
 }
 
-function getnews()
-{
-    let content=document.getElementById('main-content');
-    let str='';
-    url='http://newsapi.org/v2/top-headlines?country=in&apiKey=38f3b7aa55ca46c783055e85544e8bcd';
-    fetch(url).then((response)=>{
-        return response.text();
-    }).then((data)=>
-    {
-        let obj=JSON.parse(data);
-        obj.articles.forEach(function(element,index){  
-            str+=`<div class="accordion" id="accordionExample">
+function getnews() {
+  let content = document.getElementById('main-content');
+  let str = '';
+  url = 'http://newsapi.org/v2/top-headlines?country=in&apiKey=38f3b7aa55ca46c783055e85544e8bcd';
+  fetch(url).then((response) => {
+    return response.text();
+  }).then((data) => {
+    let obj = JSON.parse(data);
+    obj.articles.forEach(function (element, index) {
+      str += `<div class="accordion" id="accordionExample">
             <div class="card" id="news_link_title">
               <div class="card-header" id="heading${index}">
                 <h2 class="mb-0">
@@ -47,65 +44,61 @@ function getnews()
             </div>
             </div>
             <br>`
-        });
-        content.innerHTML=str;
-    })
+    });
+    content.innerHTML = str;
+    console.log(content.innerHTML);
+  })
 }
 getnews();
-function covid()
-{
-  url='https://api.rootnet.in/covid19-in/stats/latest';
-  fetch(url).then(function(response)
-  {
+function covid() {
+  url = 'https://api.rootnet.in/covid19-in/stats/latest';
+  fetch(url).then(function (response) {
     return response.text();
-}).then((data)=>{
-  let tablebody = document.getElementById('tablebody');
-  let uistr='';
-  let obj=JSON.parse(data);
-  obj.data.regional.forEach((element)=>{
-    uistr+=`<tr>
+  }).then((data) => {
+    let tablebody = document.getElementById('tablebody');
+    let uistr = '';
+    let obj = JSON.parse(data);
+    obj.data.regional.forEach((element) => {
+      uistr += `<tr>
     <td>${element["loc"]}</td>
     <td>${element["totalConfirmed"]}</td>
     <td>${element["confirmedCasesIndian"]}</td>
     <td>${element["discharged"]}</td>
     <td>${element["deaths"]}</td>
 </tr>`;
-  });
-  tablebody.innerHTML=uistr;
-})
+    });
+    tablebody.innerHTML = uistr;
+  })
 }
 covid();
 
-let signup=document.getElementById('signup');
-signup.addEventListener('click',function()
-{
-  let signup_name=document.getElementById('signup_name');
-  let signup_mail=document.getElementById('signup_mail');
-  let pswrd=document.getElementById('pswrd');
-  if(signup_name.value.length>0 && signup_mail.value.length>0 && pswrd.value.length>0)
-  {
-    let alert=document.getElementById('alertmsg');
-    alert.innerHTML=`<div class="alert alert-warning alert-dismissible fade show" role="alert">
+let signup = document.getElementById('signup');
+signup.addEventListener('click', function () {
+  let signup_name = document.getElementById('signup_name');
+  let signup_mail = document.getElementById('signup_mail');
+  let pswrd = document.getElementById('pswrd');
+  if (signup_name.value.length > 0 && signup_mail.value.length > 0 && pswrd.value.length > 0) {
+    let alert = document.getElementById('alertmsg');
+    alert.innerHTML = `<div class="alert alert-warning alert-dismissible fade show" role="alert">
     <strong>Great </strong>Succesfully Registered
     <button type="button" class="close" data-dismiss="alert" aria-label="Close">
       <span aria-hidden="true">&times;</span>
     </button>
   </div>`;
     setTimeout(function () {
-        alert.innerHTML = ``;
+      alert.innerHTML = ``;
     }, 5000);
   }
-  else
-  {
-    let alert=document.getElementById('alertmsg');
-    alert.innerHTML=`<div class="alert alert-warning alert-dismissible fade show" role="alert">
+  else {
+    let alert = document.getElementById('alertmsg');
+    alert.innerHTML = `<div class="alert alert-warning alert-dismissible fade show" role="alert">
     <strong>Error </strong>Incorrect Details
     <button type="button" class="close" data-dismiss="alert" aria-label="Close">
       <span aria-hidden="true">&times;</span>
     </button>
   </div>`;
     setTimeout(function () {
-        alert.innerHTML = ``;
+      alert.innerHTML = ``;
     }, 5000);
   }
 })
